1008cacfb0d6b2549236ec5d09a91265f60fdb10
wkynrocks <nosisomes@gmail.com>
2025-03-20 22:33:48 +0100

    Build gradle updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.wkynrocks"
@@ -10,6 +12,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.5")
+    implementation("com.h2database:h2")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.16.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
